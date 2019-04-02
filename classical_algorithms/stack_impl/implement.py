0c54588816e91a0455d6e6eff7b43e1d5e264ce5
casper_yue@163.com <casper_yue@163.com>
2019-04-02 13:25:39 +0800

    impl stack data structure

--- classical_algorithms/stack_impl/implement.py
+++ classical_algorithms/stack_impl/implement.py
@@ -8,6 +8,9 @@ class Stack(object):
     def __init__(self):
         self.top = None
 
+    def is_empty(self):
+        return self.top is None
+
     def peek(self):
         if self.top is not None:
             return self.top.item
@@ -17,19 +20,33 @@ class Stack(object):
         node = Node(item)
         node.next = self.top
         self.top = node
+        return node.item
 
     def pop(self):
-        node=self.top
-        self.top = node.next
-        return node.item
+        element = self.top
+        self.top = element.next
+        return element.item
+
+    def clear(self):
+        while self.top is not None:
+            self.pop()
+
+    def size(self):
+        count = 0
+        current_item = self.top
+        while current_item is not None:
+            current_item = current_item.next
+            count += 1
+        return count
 
 
 if __name__ == '__main__':
     stack = Stack()
-    print(stack.peek())
-    stack.push(5)
-    stack.push(4)
-    stack.push(3)
-    print(stack.peek())
+    print(stack.push(5))
+    print(stack.push(4))
+    print(stack.push(7))
+    print(stack.size())
     print(stack.pop())
-    print(stack.peek())
+    print(stack.size())
+    stack.clear()
+    print(stack.is_empty())
